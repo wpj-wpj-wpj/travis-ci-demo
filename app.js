2d9b79963f9b2beb87b20048e7cf8aee1693a852
Vincent <kaifbzsp2@gmail.com>
2018-03-11 09:59:25 +0800

    更新

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = 'Hello World!123'
+  ctx.body = 'Hello World!Vincent---'
 })
 
 app.listen(3000, () => console.log('Server is running at 3000...'))
